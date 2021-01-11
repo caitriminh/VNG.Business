cc54b121a500435fb426dc8108de0877a2dd3dfa
CÁI TRÍ MINH <38499639+caitriminh@users.noreply.github.com>
2021-01-11 09:22:17 +0700

    up

---
+++ Entities/DTA/DM_PhanKhu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VNG.Business.Entities.DTA
+{
+    public class DM_PhanKhu
+    {
+        public int ID { get; set; }
+        public string MaPhanLoai { get; set; }
+        public string MaChungLoai { get; set; }
+        public string NhomGia { get; set; }
+        public string Khu { get; set; }
+        public string IsActive { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public string NguoiTao { get; set; }
+        public DateTime? NgayThayDoi { get; set; }
+        public string NguoiThayDoi { get; set; }
+    }
+}
---
+++ Models/DTA/DM_PhanKhu_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace VNG.Business.Models.DTA
+{
+    public class DM_PhanKhu_ViewModel
+    {
+        public string action { get; set; }
+        public int ID { get; set; }
+        public string MaPhanLoai { get; set; }
+        public string MaChungLoai { get; set; }
+        public string NhomGia { get; set; }
+        public string Khu { get; set; }
+        public string IsActive { get; set; }
+        public string NguoiTao { get; set; }
+        public string NguoiThayDoi { get; set; }
+    }
+}
